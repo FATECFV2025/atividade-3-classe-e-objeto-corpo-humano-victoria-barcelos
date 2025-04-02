c71f872fcea6d1e65fe02a791f45fee3b730b602
Victoria Caroline <contato.victoriab1@gmail.com>
2025-04-02 12:30:52 -0300

    Add files via upload

---
+++ corpohumano.java
@@ -0,0 +1,24 @@
+package atividades;
+
+
+public class corpohumano {
+    private double massa;
+    private double altura;
+    private double densidade;
+
+
+    public void corpohumano (double massa, double altura){
+        this.massa=massa;
+        this.altura=altura;
+    }
+
+
+    public void setMassa(double massa){
+        this.massa=massa;
+    }
+
+    public void setAltura(double altura){
+
+    }
+    
+}
